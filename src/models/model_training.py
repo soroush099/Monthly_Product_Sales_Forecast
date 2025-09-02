78ec2ace5791a96eac2d1029f65f7a6342f0d629
soroush099 <141868381+soroush099@users.noreply.github.com>
2025-09-02 10:40:24 +0330

    Fix typo in function argument

--- src/models/model_training.py
+++ src/models/model_training.py
@@ -1,12 +1,12 @@
 from sklearn.ensemble import RandomForestRegressor
 
 
-def train_model(X, y):
+def train_model(x, y):
     rf_model = RandomForestRegressor(
         n_estimators=100,
         random_state=42,
         n_jobs=-1,
         min_samples_leaf=5
     )
-    rf_model.fit(X, y)
+    rf_model.fit(x, y)
     return rf_model
